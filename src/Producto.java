c8bfce26d66d2e3f10ed382485f054d95324be7c
Laghed <gabjisar2@gmail.com>
2024-11-26 19:44:13 -0500

    Se agrego la clase Producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,7 +1,57 @@
 public class Producto {
-    int id;
-    String nombre;
-    int cantidad;
-    int aaa;
+    // Atributos de la clase Producto
+    private int id;
+    private String nombre;
+    private int cantidad;
+    private double precio;
 
+    // Constructor
+    public Producto(int id, String nombre, int cantidad, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // Método para mostrar información del producto
+    public void mostrarInformacion() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Precio: $" + precio);
+    }
 }
+
