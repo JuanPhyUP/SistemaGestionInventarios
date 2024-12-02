aa1c09b05472515c7754c2b0532bb3fbf00d3032
PHY-INC <juanccelislozada13@gmail.com>
2024-12-01 19:26:41 -0500

    Creación de la clase Inventari0

---
+++ src/Inventario.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Inventario {
+    //Atributo
+    ArrayList<Producto> producto;
+
+    //Metodo constructor
+    public Inventario (ArrayList producto){
+        this.producto = producto;
+    }
+
+
+
+}
